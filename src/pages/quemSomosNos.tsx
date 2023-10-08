f1ba2eceb50aa08f2cda0665f5da97b95e31061f
ThiagoLD02 <infothiago@unifei.edu.br>
2023-10-08 14:44:45 -0300

    Add: Adicionando imagens dos membros, logo no titulo da pagina, imagem de background na pagina onde estamos

--- src/pages/quemSomosNos.tsx
+++ src/pages/quemSomosNos.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
+import a from "../assets/background1.jpg";
 import equipe from "../assets/equipe-new-2023-1036x691.jpeg";
 import newarmstrongLogo from "../assets/newarmstrong-logo-1076x605.png";
 
 export function QuemSomosNos() {
+  useEffect(() => {
+    document.title = "Quem somos nós?";
+  }, []);
   return (
     <div className="mx-auto h-full w-full max-w-sm lg:max-w-7xl flex flex-col text-center">
       <div className="grid grid-cols-1 xl:grid-cols-2 justify-items-center items-center mb-10 xl:mb-0">
@@ -37,16 +42,22 @@ export function QuemSomosNos() {
           beneficiários a oportunidade de melhorar sua qualidade de vida.
         </p>
       </div>
-
-      <div className="flex flex-col w-full text-center items-center justify-center mt-6 bg-azulClaro text-white h-[500px] mb-48">
-        <h1 className="text-center font-bold size text-5xl text-white">
-          Onde estamos
-        </h1>
-        <iframe
-          className="w-11/12 h-3/4 pt-10"
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB7QWkvlxV39ZsyKvhZRjPGVqlJFvxxNkw&amp;q=UNIFEI Itabira Brazil"
-          allowFullScreen={true}
-        ></iframe>
+      <div
+        style={{
+          backgroundImage: `url(${a})`,
+        }}
+        className="w-full mt-6 text-white h-[500px] mb-48 bg-cover bg-no-repeat"
+      >
+        <div className="flex flex-col w-full text-center items-center opacity-90 bg-azulEscuro justify-center text-white h-full ">
+          <h1 className="text-center font-bold size text-5xl text-white">
+            Onde estamos
+          </h1>
+          <iframe
+            className="w-11/12 h-3/4 pt-10"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB7QWkvlxV39ZsyKvhZRjPGVqlJFvxxNkw&amp;q=UNIFEI Itabira Brazil"
+            allowFullScreen={true}
+          ></iframe>
+        </div>
       </div>
     </div>
   );
